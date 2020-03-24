93aef9fb32e96da61b3f4bb7060f2c89f767adb3
ben06feb <ben.06feb@gmail.com>
2020-03-24 18:23:51 +0530

    Initial
    Initial commit

---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    float speed;
+    public GameObject world;
+    public float areaLimit = 10f;
+
+    void Update()
+    {
+        speed += Time.deltaTime / 100;
+        transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z - speed);
+
+        float distanceFromOrgin = Vector3.Distance(transform.position, Vector3.zero);
+        Vector3 direction = transform.position - Vector3.zero;
+
+        if(distanceFromOrgin >= areaLimit)
+        {
+            transform.position = Vector3.zero;
+            world.transform.position += direction * -1;
+        }
+    }
+}
